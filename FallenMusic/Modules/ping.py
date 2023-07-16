6f839d6c2e9c3a443d81de1ea3699ff7cfe86fef
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2023-07-16 23:13:40 +0530

    Update ping.py
    Signed-off-by: Devilking12 <128044704+Devilking12@users.noreply.github.com>

--- FallenMusic/Modules/ping.py
+++ FallenMusic/Modules/ping.py
@@ -46,21 +46,21 @@ async def ping_fallen(_, message: Message):
     uptime = get_readable_time((upt))
 
     await hmm.edit_text(
-        f"""➻ ᴩᴏɴɢ : `{resp}ᴍs`
+        f"""➻ ᴩᴏɴɢ : {resp}ᴍs
 
 <b><u>{BOT_NAME} sʏsᴛᴇᴍ sᴛᴀᴛs :</u></b>
 
-๏ **ᴜᴩᴛɪᴍᴇ :** {uptime}
-๏ **ʀᴀᴍ :** {mem}
-๏ **ᴄᴩᴜ :** {cpu}
-๏ **ᴅɪsᴋ :** {disk}""",
+๏ ᴜᴩᴛɪᴍᴇ : {uptime}
+๏ ʀᴀᴍ : {mem}
+๏ ᴄᴩᴜ : {cpu}
+๏ ᴅɪsᴋ : {disk}""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("❄ sᴜᴘᴘᴏʀᴛ ❄", url=config.SUPPORT_CHAT),
                     InlineKeyboardButton(
                         "✨ sᴏᴜʀᴄᴇ ✨",
-                        url="https://github.com/Devilking12/FallenMusic",
+                        url="https://t.me/Mr_selfish_person",
                     ),
                 ],
             ]
